727cc1bcc20ebef27cc3d5f13b05a66381a0ce93
rzfd <36652748+rzfd@users.noreply.github.com>
2023-03-20 00:38:29 +0700

    1 file modified

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import './style.css';
 // Write Javascript code!
 // const appDiv = document.getElementById('app');
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
-function hitungLuas() {
-  const alas = document.getElementById(`alas`).value;
-  const tinggi = document.getElementById(`tinggi`).value;
+const hitungLuas = () => {
+  const alas = document.getElementById('alas').value;
+  const tinggi = document.getElementById('tinggi').value;
   const luas = 0.5 * alas * tinggi;
-  document.getElementById(`luas`).values = luas;
-}
+  document.getElementById('luas').value = luas;
+};
 
-const button = document.getElementById(`buttonHitung`);
-button.addEventListener(`click`, hitungLuas);
+const button = document.getElementById('buttonHitung');
+button.addEventListener('click', hitungLuas);
